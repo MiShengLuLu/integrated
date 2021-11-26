8ada12ffa9430061f894a3b9156968cf8fc09847
celina520524 <celina520524@163.com>
2021-11-26 22:15:56 +0800

    react-router

--- react/src/integrated-react.js
+++ react/src/integrated-react.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
 
+// 指定根组件的渲染位置
+const domElementGetter = () => document.getElementById("root")
+
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: Root,
+  domElementGetter,
   errorBoundary(err, info, props) {
     // Customize the root error boundary for your microfrontend here.
     return null;
--- react/src/root.component.js
+++ react/src/root.component.js
@@ -1,3 +1,21 @@
+// export default function Root(props) {
+//   return <section>{props.name} is mounted!</section>;
+// }
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './views/Home'
+import About from './views/About'
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return <BrowserRouter basename="/react">
+    <div>
+      <Link to="/"></Link>
+      <Link to="/about"></Link>
+    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
+  </BrowserRouter>;
 }
+
